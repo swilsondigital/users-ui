78b1c7fff5fccefc04a037c4ebb2064b488d7984
swilsondigital <swilsondigital@gmail.com>
2022-02-14 15:36:45 -0600

    pushing for vercel fix

--- pages/clients/[clientID]/projects/create.js
+++ pages/clients/[clientID]/projects/create.js
@@ -1,4 +1,4 @@
 export default function CreateClientProject({client}) {
     // render project creation form
-    return (<div>Client Project Creation Form</div>);
+    return (<div>Client Project Creation Form Here</div>);
 }
