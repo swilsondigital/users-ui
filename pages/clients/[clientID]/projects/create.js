2a43154f2ef2f705265924f935704e15ba2dae62
swilsondigital <swilsondigital@gmail.com>
2022-02-14 15:17:47 -0600

    fixed create static paths, for now

--- pages/clients/[clientID]/projects/create.js
+++ pages/clients/[clientID]/projects/create.js
@@ -2,23 +2,3 @@ export default function CreateClientProject({client}) {
     // render project creation form
     return (<div>Client Project Creation Form</div>);
 }
-
-export async function getStaticPaths(){
-    // get dynamic routes from api
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/clients`)
-    const clients = await res.json()
-
-    const paths = clients.map((client) => ({
-        params: { clientID: client.ID.toString() }
-    }))
-
-    return {paths, fallback: false }
-}
-
-export async function getStaticProps({ params }){
-    // get client from api
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/clients/${params.clientID}`)
-    const client = await res.json()
-
-    return { props: {client} }
-}
